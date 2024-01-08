921874cc56a039d32d923b7daf0a3e7991fd3d2b
carlacm7 <155462334+carlacm7@users.noreply.github.com>
2024-01-08 14:46:31 +0000

    semana 1

---
+++ Semana 1/estructudasdeControl.js
@@ -0,0 +1,33 @@
+var numero = 13;
+if (numero % 2 === 0) {
+    console.log(numero + ' es par');
+} else {
+    console.log(numero + ' no es par');
+}
+if (numero % 5 === 0) {
+    console.log(numero + ' es multiplo de 5');
+} else {
+    console.log(numero + " No es multiplo de 5");
+}
+
+
+for (let i = 0; i <= 5; i++) {
+    console.log(i);
+}
+
+for (let i = 10; i > 0; i--) {
+    console.log(i);
+}
+
+let cont = 0;
+while (cont < 5) {
+    cont++;
+    console.log(cont);
+}
+let conta = 10;
+while (conta > 0) {
+
+    console.log(conta);
+    conta--;
+
+}
