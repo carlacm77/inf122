88f1464882fe3a645f71a87483dc641829987218
carlacm7 <155462334+carlacm7@users.noreply.github.com>
2024-01-23 02:58:18 +0000

    tarea

--- react/tres-en-raya/src/components/Juego.js
+++ react/tres-en-raya/src/components/Juego.js
@@ -4,42 +4,67 @@ import Historial from './Historial';
 import '../styles/Juego.css';
 import { useState } from 'react';
 
+let jug=0;
+let ganador="N"
+//let [ganador, setGanador] = useState("N");
 function Juego() {
     const [historial, setHistorial] = useState([
         {cuadros: Array(9).fill(null)},
     ]);
     const [nroMovimiento, setNroMovimiento] = useState(0);
-
+    const [movJug, setmovJug] = useState(Array(9).fill(0));
     const [cuadros, setCuadros] = useState(Array(9).fill(null));
     const [jugador, setJugador] = useState("X");
-    const [ganador, setGanador] = useState(null);
+    
+    
+    
     const click = (i) => {
         const nuevoMovimiento = historial.slice(0, nroMovimiento + 1);
         const movimientoActual = nuevoMovimiento[nuevoMovimiento.length - 1];
         const cuadros = movimientoActual.cuadros.slice();
-        if (cuadros[i] === null) {
+        if (cuadros[i] === null && movJug[jug]===0) {
+            
             cuadros[i] = jugador;
             setCuadros(cuadros);
             setJugador(jugador === "X" ? "O" : "X");
             setHistorial(nuevoMovimiento.concat([{ cuadros }]));
             setNroMovimiento(nuevoMovimiento.length);
+            jug++;
         }
         if (calcularGanador(cuadros) !== null) {
-            setGanador(calcularGanador(cuadros));
+            ganador=(calcularGanador(cuadros));
+            movJug[jug]=1;
         }
 
     }
     const saltarA = (movimiento) => {
+        jug=movimiento;
+        console.log("jug", jug);
         console.log("movimiento", movimiento);
         setNroMovimiento(movimiento);
-        setJugador(jugador === "X" ? "O" : "X");
+        setJugador(jug%2 === 1 ? "O" : "X");
+        if(movJug[jug]===0){
+            for (let ii = 0; ii <=9; ii++){
+                movJug[ii]=0;
+            }
+            ganador="N";
+        }
+        if (movimiento===0){
+            setHistorial([{cuadros: Array(9).fill(null)},]);
+            setNroMovimiento(0);
+            setmovJug(Array(9).fill(0));
+            setCuadros(Array(9).fill(null));
+            setJugador("X"); 
+        }
 
     }
     const movimientoActual = historial[nroMovimiento];
+    console.log("gan",ganador);
     return (
+        
         <div className="juego">
             <div className="juego-tablero">
-                <h2>{ganador ? `Ganador: ${ganador}` : `Próximo jugador: ${jugador}`}</h2>
+                <h2>{ ganador !== "N" ? `Ganador: ${ganador}` : `Próximo jugador: ${jugador}`}</h2>
                 <Tablero cuadros={movimientoActual.cuadros} onClick={(i) => click(i)} />
             </div>
             <Historial historial={historial} saltarA={saltarA} />
