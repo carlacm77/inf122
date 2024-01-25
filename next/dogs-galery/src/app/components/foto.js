39f69f7f9d9c853db4f22ef440074c5cccea9ed2
carlacm77 <157501725+carlacm77@users.noreply.github.com>
2024-01-25 15:37:01 +0000

    pokemon

--- next/dogs-galery/src/app/components/foto.js
+++ next/dogs-galery/src/app/components/foto.js
@@ -4,16 +4,22 @@ import style from "./foto.module.css";
 import { useState, useEffect } from "react";
 
 function Foto(){
-    const [perrito, setPerrito]=useState("/vercel.svg")
-    const url="https://dog.ceo/api/breed/waterdog/spanish/images/random";
+    const [perrito, setPerrito]=useState("/vercel.svg");
+    const [estado, setEstado]=useState("esperando");
+    const [habilidad, setHabilidad]=useState("habilidad");
+    const url="https://pokeapi.co/api/v2/pokemon/eevee";
 
     useEffect(()=>{                               
-        fetch(url).then(res=>res.json()).then(data=>{setPerrito(data.message)})                                         
+        fetch(url).then(res=>res.json()).then(data=>{setPerrito(data.sprites.front_default), 
+            setEstado(data.species.name), 
+            setHabilidad(data.abilities[0].ability.name)})                                         
     },[]);
 
     return(
         <div className={style.contenedor}>
-            <Image src={perrito} alt="" width={250} height={250}/>
+            <h1>{estado}</h1>
+            <Image src={perrito} alt="" width={400} height={400}/>
+            <h1>{habilidad}</h1>
         </div>
     );
 }
