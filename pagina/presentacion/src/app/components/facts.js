64a53113c5248e31939b61da1998304b28b2c919
carlacm77 <157501725+carlacm77@users.noreply.github.com>
2024-01-27 05:52:31 +0000

    portafolio

--- pagina/presentacion/src/app/components/facts.js
+++ pagina/presentacion/src/app/components/facts.js
@@ -4,9 +4,9 @@ import FactItem from "./factItem";
 function Facts(){
     return (
         <div className={style.facts}>
-            <FactItem number="7" description="Years of work experience"/>
-            <FactItem number="50+" description="Completed projects"/>
-            <FactItem number="20+" description="Satisfied customers"/>
+            <FactItem number="1" description="Years of experience"/>
+            <FactItem number="2" description="Completed projects"/>
+            <FactItem number="#" description="Satisfied customers"/>
         </div>
     );
 }
--- pagina/presentacion/src/app/components/projects.js
+++ pagina/presentacion/src/app/components/projects.js
@@ -5,11 +5,11 @@ import ProjectDone from "./projectDone"
 function Projects(){
     return (
         <div className={style["project-container"]}>
-            <ProjectDone proyecto="metas.png" titulo="Lista de Metas" descripcion="Landing page of SmarterTrading411 created using WordPress"/>
+            <ProjectDone proyecto="metas.png" titulo="Mis Metas" descripcion="Registra tus metas y controla cuantan estan pendientes o completadas"/>
             <Project gris={false} proyecto="construccion.jpg"/>
             <Project gris={true} proyecto="construccion.jpg"/>
-            <Project gris={false} proyecto="construccion.jpg"/>
-            <Project gris={false} proyecto="construccion.jpg"/>
+            <Project gris={true} proyecto="construccion.jpg"/>
+            <Project gris={true} proyecto="construccion.jpg"/>
             <Project gris={true} proyecto="construccion.jpg"/>
         </div>
     );
